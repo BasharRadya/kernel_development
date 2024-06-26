afea224b1fe00d9af2bd75f7dead19cee3caba80
bashar-osama@spring2024-iit.kdlp.underground.software <bashar-osama@spring2024-iit.kdlp.underground.software>
2024-06-26 15:03:55 +0300

    lvl0 Completed:
    As instructed, lvl0 is Complete.
    
    Signed-off-by: bashar-osama <bashar-osama@spring2024-iit.kdlp.underground.software>

---
+++ bashar-osama/P0/main.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//char *getcwd(char buf[.size], size_t size);
+char *get_current_dir_name(void);
+
+int main(void)
+{
+	char *pwd = get_current_dir_name();
+
+	printf("%s$\n ", pwd);
+	free(pwd);
+	return 0;
+}
