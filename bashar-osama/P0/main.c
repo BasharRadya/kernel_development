0464a3d7eace161df6406c7a424aa67ef6c46c9a
bashar-osama@spring2024-iit.kdlp.underground.software <bashar-osama@spring2024-iit.kdlp.underground.software>
2024-06-26 15:03:56 +0300

    lvl1 Completed:
    Completed lvl1 as isntructed
    
    Signed-off-by: bashar-osama <bashar-osama@spring2024-iit.kdlp.underground.software>

--- bashar-osama/P0/main.c
+++ bashar-osama/P0/main.c
@@ -1,17 +1,36 @@
 // SPDX-License-Identifier: MIT
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-//char *getcwd(char buf[.size], size_t size);
 char *get_current_dir_name(void);
+
+void print_pwd(void)
+{
+	char *pwd = get_current_dir_name();
+	printf("%s$ ", pwd);
+	free(pwd);
+}
+
+void get_input(void)
+{
+	char *input = NULL; //force getline to allloc it
+	size_t len = 0;
+	ssize_t byteread = getline(&input, &len, stdin);
+
+	if (byteread == -1) {
+		exit(0);
+	}
+	free(input);
+}
 
 int main(void)
 {
-	char *pwd = get_current_dir_name();
-
-	printf("%s$\n ", pwd);
-	free(pwd);
+	while (1) {
+		print_pwd();
+		get_input();
+	}
 	return 0;
 }
